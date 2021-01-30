0ac133a8237f7099a1b96ba47848784552594700
vinod <vinodkumarmahato1@gmail.com>
2021-01-30 11:50:58 +0530

    Drum Kit

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("keydown", function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();   
+    key.classList.add("playing") 
+     
+});
+
+
+const keys = document.querySelectorAll(".key");
+        keys.forEach( key => key.addEventListener("transitionend", function(e){
+            console.log(e)
+                if(e.propertyName !== "transform") return;
+                    this.classList.remove("playing");
+        }));    
+
+document.addEventListener("click", function(e){
+    const audio = document.querySelector(`audio[data-key="${e.target.id}"]`);
+    const key = document.querySelector(`.key[data-key="${e.target.id}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();   
+    key.classList.add("playing") 
+   
+});
+
